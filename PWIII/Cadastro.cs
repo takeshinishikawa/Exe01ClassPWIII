2a7c010cf8209109369b5ec043873112a8afeb8e
takeshinishikawa <takeshinishikawa@gmail.com>
2022-08-29 18:22:10 -0300

    Add project files.

---
+++ PWIII/Cadastro.cs
@@ -0,0 +1,16 @@
+namespace PWIII
+{
+    public class Cadastro
+    {
+
+        public string Cpf { get; set; }
+
+        public string Nome { get; set; }
+
+
+        public DateTime DataNascimento { get; set; }
+        public int Idade { get; set; }
+
+
+    }
+}
---
+++ PWIII/Controllers/Cadastro.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PWIII.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Cadastro : ControllerBase
+    {
+        /*private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };*/
+
+        //private readonly ILogger<Cadastro> _logger;
+
+        public List<PWIII.Cadastro> cadastros { get; set; }
+
+        private readonly ILogger<Cadastro> _logger;
+
+
+        public Cadastro(ILogger<Cadastro> logger)
+        {
+            _logger = logger;
+            cadastros = new List<PWIII.Cadastro>();
+        }
+
+        [HttpGet]
+        //https://localhost:7197/Cadastro GET
+        public IEnumerable<PWIII.Cadastro> Consulta()
+        {
+            return cadastros.ToList();
+        }
+        [HttpPost]
+        //https://localhost:7197/Cadastro POST
+        public PWIII.Cadastro Insert(PWIII.Cadastro cadastro)
+        {
+            cadastros.Add(cadastro);
+            return cadastro;
+        }
+
+        [HttpDelete]
+        //https://localhost:7197/Cadastro DELETE
+        public PWIII.Cadastro Delete(PWIII.Cadastro cadastro)
+        {
+            cadastros.Remove(cadastro);
+            return cadastro;
+        }
+
+        [HttpPut]
+        //https://localhost:7197/Cadastro PUT
+        public PWIII.Cadastro Update(PWIII.Cadastro cadastro)
+        {
+            PWIII.Cadastro item = cadastros.FirstOrDefault(x => x.Cpf == cadastro.Cpf);
+            if (item != null)
+                item = cadastro;
+
+            return cadastro;
+        }
+
+    }
+}
